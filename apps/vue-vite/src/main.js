b0a2ec3e7bf1fb0fb148e75672f009421eb31de2
Jason <jcubic@gmail.com>
2022-03-06 17:48:30 -0800

    clean up

--- apps/vue-vite/src/main.js
+++ apps/vue-vite/src/main.js
@@ -1,8 +1,5 @@
 import { createApp } from 'vue';
-// import 'bootstrap/dist/css/bootstrap.css';
-
 import './assets/scss/bootstrap.scss';
-
 import App from './App.vue';
 
 createApp(App).mount('#app');
--- apps/vue-vite/vite.config.js
+++ apps/vue-vite/vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite';
 import { fileURLToPath, URL } from 'url';
 import vue from '@vitejs/plugin-vue';
 
-// import { resolve } from 'path';
-// import pkg from './package.json';
-
 const config = defineConfig({
   base: '/dtna-sxsw-survey/',
   plugins: [vue()],
@@ -13,24 +10,6 @@ const config = defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  // build: {
-  //   lib: {
-  //     entry: resolve(__dirname, './src/lib.js'),
-  //     name: pkg.name,
-  //   },
-  //   rollupOptions: {
-  //     // make sure to externalize deps that shouldn't be bundled
-  //     // into your library
-  //     external: ['vue'],
-  //     output: {
-  //       // Provide global variables to use in the UMD build
-  //       // for externalized deps
-  //       globals: {
-  //         vue: 'Vue',
-  //       },
-  //     },
-  //   },
-  // },
 });
 
 export default config;
